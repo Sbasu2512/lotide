67b8d2b9d42d40f7817d645011ab92e17dd3e1ee
Sayantan Basu <sayantanbasu@live.in>
2021-05-24 20:29:34 -0400

    program runs

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
+    );
+  }
+};
+
+const head = arr => {
+ return arr[0];
+}
+
+//syntax
+head([4,5,2,3,1,9,6]);
+//Testing our code
+assertEqual(head([3,1,9,6]), 3);
+assertEqual(head([5]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
