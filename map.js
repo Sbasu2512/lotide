55dd4175a43673fa863f80428e23eb8d19b56787
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 17:32:46 -0400

    added new functionality in the file: map.js

--- map.js
+++ map.js
@@ -27,7 +27,6 @@ const results1 = map(words, word => word[0]);
 console.log(results1);
 */
 const words = ["ground", "control", "to", "major", "tom"];
-//Call map and pass in our words and a simple arrow function.
 const map = (array,callback) => {
 // temporary code:
 //console.log('array: ', array);
