15976b3622f61c1d01bcec3bf31f323b41e3bfa0
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 17:24:53 -0400

    added new functionality in the file: map.js

--- map.js
+++ map.js
@@ -17,6 +17,10 @@ for (let item of array) {
   console.log('item BEFORE: ', item);
   console.log('item AFTER: ', callback(item));
   console.log('---');
+  results.push(callback(item));
 }
   return results;
 } ;
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
