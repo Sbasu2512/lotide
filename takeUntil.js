e1f624bad6eab5604e2285516afde68dab529c14
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 18:42:46 -0400

    added new functionality to file: takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -10,9 +10,9 @@ Implement takeUntil which will keep collecting items from a provided array until
 const takeUntil = function(arr, callback) {
   var result = [] ;
   for (let item of arr) {
-    console.log('item BEFORE: ', item);
-  console.log('item AFTER: ', callback(item));
-  console.log('---');
+   // console.log('item BEFORE: ', item);
+ // console.log('item AFTER: ', callback(item));
+ // console.log('---');
   if(callback(item) === false){
     result.push(item);
   } else {
