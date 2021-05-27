dadc79b85ba960c7712d2a539ecc70ab567037f9
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 18:10:03 -0400

    added new functionality to file: takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -19,4 +19,4 @@ console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+console.log(results2);  //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
