ea2fe87139a925d4ab3eca8462d541c9384468ad
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 17:30:09 -0400

    added new functionality in the file: map.js

--- map.js
+++ map.js
@@ -5,14 +5,16 @@ An array to map
 A callback function
 The map function will return a new array based on the results of the callback function.
 */
+//Documentation of how!
+/**/
 const words = ["ground", "control", "to", "major", "tom"];
 //Call map and pass in our words and a simple arrow function.
 const map = (array,callback) => {
 // temporary code:
 //console.log('array: ', array);
 //console.log('callback: ', callback);
-
 const results = [];
+//We now need to populate our results array 
 for (let item of array) {
   //console.log('item BEFORE: ', item);
   //console.log('item AFTER: ', callback(item));
