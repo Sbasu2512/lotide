c1a3c8bd21077d19682584a0a6690e8c5363f774
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 17:32:34 -0400

    added new functionality in the file: map.js

--- map.js
+++ map.js
@@ -26,3 +26,19 @@ for (let item of array) {
 const results1 = map(words, word => word[0]);
 console.log(results1);
 */
+const words = ["ground", "control", "to", "major", "tom"];
+//Call map and pass in our words and a simple arrow function.
+const map = (array,callback) => {
+// temporary code:
+//console.log('array: ', array);
+//console.log('callback: ', callback);
+const results = [];
+//We now need to populate our results array & Complete the for loop by pushing each item into our results array. And that should complete the logic for our map function!
+for (let item of array) {
+  console.log('item BEFORE: ', item);
+  console.log('item AFTER: ', callback(item));
+  console.log('---');
+  results.push(callback(item));
+}
+  return results;
+} ;
