9c175b49b66eb5769d365c119750d4d185bd27f7
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 17:14:35 -0400

    added new functionality in the file: map.js

--- map.js
+++ map.js
@@ -1,1 +1,8 @@
-//
+/*
+Our map function will take in two arguments:
+
+An array to map
+A callback function
+The map function will return a new array based on the results of the callback function.
+*/
+const words = ["ground", "control", "to", "major", "tom"];
