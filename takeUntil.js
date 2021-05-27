9d377bd0a0eb0c686a368de3732b27ea9baa6926
Sayantan Basu <sayantanbasu@live.in>
2021-05-27 18:09:15 -0400

    added new functionality to file: takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -4,6 +4,7 @@ The array to work with
 The callback (which Lodash calls "predicate")
 The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
 To keep things simple, the callback should only be provided one value: The item in the array.
+Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
 */
 
 const takeUntil = function(array, callback) {
