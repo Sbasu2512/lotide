1cffd2007d553e79d7eb29853182895faaf68dd7
Sayantan Basu <sayantanbasu@live.in>
2021-05-28 01:13:40 -0400

    added a fn: countOnly

--- countOnly.js
+++ countOnly.js
@@ -11,4 +11,5 @@ const assertEqual = function(actual, expected) {
 };
 
 //countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
-//
+const countOnly = function(allItems, itemsToCount) {
+}
