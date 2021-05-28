033a291390fec14abf68e5b3c0701e8b066a6986
Sayantan Basu <sayantanbasu@live.in>
2021-05-28 01:12:12 -0400

    added the assert fn: countOnly

---
+++ countOnly.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
+    );
+  }
+};
