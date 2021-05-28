372343221e9a7d82af8c8044949edfba8e02ac54
Sayantan Basu <sayantanbasu@live.in>
2021-05-28 10:52:38 -0400

    created a new file

---
+++ countLetters.js
@@ -0,0 +1,6 @@
+/*Create a function called countLetters.
+
+The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+
+For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
+*/
