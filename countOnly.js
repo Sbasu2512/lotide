81991c4e5bc1a7bc29d10e9cee762f430be47b80
Sayantan Basu <sayantanbasu@live.in>
2021-05-28 01:20:24 -0400

    added fn

--- countOnly.js
+++ countOnly.js
@@ -11,5 +11,30 @@ const assertEqual = function(actual, expected) {
 };
 
 //countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+//Our function countOnly needs to return a proper report on all the strings found in the input array, and their respective counts. 
+//Therefore it will need to return an object that can represent the stats.
 const countOnly = function(allItems, itemsToCount) {
+  for(let item in itemsToCount) {
+    let counter = 0;
+    for(let index in allItems){
+    if(item === index) {
+      counter++;
+    }
+  }
+  }
+  return `${item}: ${counter} `  ;
 }
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(firstNames, Fang);
