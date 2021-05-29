20c7a574e684fa8ef286f9d21e9a6e0838764983
Sayantan Basu <sayantanbasu@live.in>
2021-05-29 10:30:34 -0400

    half done

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,34 @@
-const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  return results;
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
 };
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+const letterPositions = function(sentence) {
+  let result = {};
+  const final = {};
+  // logic to update results here
+  for (let letter of sentence) {
+    if (result[letter]) {
+      final[letter] += 1;
+    } else {
+      final[letter] = 1;
+    }
+  }
+ console.log(typeof(final));
+  return final ;
+};
+
+console.log(letterPositions("Hello"));
