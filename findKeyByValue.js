7d1bcc1aa2357170d79497da75bc9e8a16986c8e
Sayantan Basu <sayantanbasu@live.in>
2021-05-29 10:37:27 -0400

    half done

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
+    );
+  }
+};
+
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
