2957b0361d97ea901b6343fffd18a76bd667b743
Sayantan Basu <sayantanbasu@live.in>
2021-05-30 02:30:26 -0400

    recursive fn added to flatten.js

---
+++ flatten.js
@@ -0,0 +1,14 @@
+/*
+Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
+*/
+const flatten = function(array) {
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      flatten(item);
+    } else {
+      console.log(item);
+    }
+  });
+}
+
