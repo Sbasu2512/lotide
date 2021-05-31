40090bb06516621a7c56195dbf0c63c24b483c76
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 13:49:02 -0400

    added module functionality

--- takeUntil.js
+++ takeUntil.js
@@ -6,7 +6,6 @@ The function will return a "slice of the array with elements taken from the begi
 To keep things simple, the callback should only be provided one value: The item in the array.
 Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
 */
-
 const takeUntil = function(arr, callback) {
   var result = [] ;
   for(let e of arr) {
@@ -26,6 +25,7 @@ const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log("and the result is: ", results1);  //[ 1, 2, 5, 7, 2 ]
 */
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);  //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);  //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+module.exports = takeUntil;
