c4d08f1779b0e33887ac15032c56dcd7d6c52de5
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 13:03:45 -0400

    added assertEqual

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
-  assertArraysEqual: assertArraysEqual
+  assertArraysEqual: assertArraysEqual,
+ assertEqual: assertEqual
 };
