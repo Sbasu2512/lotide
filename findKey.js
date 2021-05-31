320b1f49294bb5b4e3ebb2b6e07e1d8f209cc1a4
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 13:24:25 -0400

    added module export fn

--- findKey.js
+++ findKey.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(
-      `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
-    );
-  } else {
-    console.log(
-      `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
-    );
-  }
-};
 //Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 const findKey = (obj,callback) => {
   let temp = [];
@@ -20,11 +9,12 @@ const findKey = (obj,callback) => {
       }
     }
   }
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+module.exports = findKey;
+// findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2) // => "noma"
