55862ea55eb8b8dbd3820e19f66b7e6e9981b497
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 13:20:18 -0400

    added multiple fns

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
+const {assertArraysEquals} = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eArrays');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
-  assertArraysEqual: assertArraysEqual,
- assertEqual: assertEqual
+  assertArraysEquals: assertArraysEquals,
+ assertEqual: assertEqual,
+ assertObjectsEqual: assertObjectsEqual,
+ countLetters: countLetters,
+ countOnly: countOnly,
+ eqArrays: eqArrays
 };
