9c4256963e93fabcf724e212043db0f72a2a43c8
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 08:53:24 -0400

    test run

--- test/headTest.js
+++ test/headTest.js
@@ -5,6 +5,11 @@ describe ("#head", () => {
   it("returns 1 for [1,2,3,4]", () => {
     assert.strictEqual(head([1,2,3,4]),1);
   });
-
+  it("returns undefined for []",() =>{
+    assert.equal(head([]), undefined);
+  });
+  it("retuns 5 for [5]", () =>{
+assert.strictEqual(head([5]), 5);
+  });
   
 });
