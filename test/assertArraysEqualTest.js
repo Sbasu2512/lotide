c828fe792d791d03ed0f0feeedc53062d8f80cfd
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 08:06:58 -0400

    refractoring in progress

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,16 @@
+const assertEqual = require('../assertEqual');
+const assertArraysEquals = require('../assertArraysEqual');
+
+console.log(assertArraysEquals([1, 2, 3], [1, 2, 3]));    // => true
+console.log();
+console.log(assertArraysEquals([1, 2, 3], [3, 2, 1]));  // => false
+console.log();
+console.log(assertArraysEquals(["1", "2", "3"], ["1", "2", "3"]));  // => true
+console.log();
+console.log(assertArraysEquals(["1", "2", "3"], ["1", "2", 3]));  // => false
+
+assertEqual(assertArraysEquals([1, 2, 3], [1, 2, 3]), true);
+assertEqual(assertArraysEquals([1, 2, 3], [1, 2, 9]), false);
+assertEqual(assertArraysEquals([1, 2, 3], [1, 2]), false);
+assertEqual(assertArraysEquals([1, 2, 3], [1, 2, '3']), false);
+assertEqual(assertArraysEquals([1, '2', 3], [1, '2', 3]), true);
