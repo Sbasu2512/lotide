07bf4c8d7ebd5b5d106ea062922f8684287c91be
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 22:29:15 -0400

    final commit

--- without.js
+++ without.js
@@ -1,29 +1,29 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(
-      `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
-    );
-  } else {
-    console.log(
-      `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
-    );
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(
+//       `🤩🤩🤩🤩  Assertion passed: ${actual} === ${expected} 💯💯💯💯`
+//     );
+//   } else {
+//     console.log(
+//       `🛑🛑🛑🛑  Assertion failed: ${actual} === ${expected} 🛑🛑🛑🛑`
+//     );
+//   }
+// };
 
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
-function eqArrays(a,b) {
-  if(a.length!=b.length)
-  return false;
- else
- {
- // comapring each element of array
-  for(var i=0;i<a.length;i++)
-  if(a[i]!==b[i])
-   return false;
-   return true;
- }
-}
+// function eqArrays(a,b) {
+//   if(a.length!=b.length)
+//   return false;
+//  else
+//  {
+//  // comapring each element of array
+//   for(var i=0;i<a.length;i++)
+//   if(a[i]!==b[i])
+//    return false;
+//    return true;
+//  }
+// }
 //Implement without which will return a subset of a given array, removing unwanted element
 //This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
 const without = function(source,itemsToRemove){
