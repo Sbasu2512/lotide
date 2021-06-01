d311a8a616285561d3d41a2cbda67c659b6eed9a
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 21:44:01 -0400

    one more test added

--- test/titleCaseTest.js
+++ test/titleCaseTest.js
@@ -14,9 +14,21 @@ const input = titleCase("this is an example");
 const output = "This Is An Example";
 assert.equal(input,output);
 });
-});
 // titleCase("test") //should return "Test"
+it("",() => {
+  const input = titleCase("test");
+  const output = "Test" ;
+  assert.equal(input,output);
+});
 // titleCase("i r cool") //should return "I R Cool"
+it("", () => {
+  const input = titleCase("i r cool");
+  const output = "I R Cool";
+  assert.equal(input,output);
+})
+});
+
+
 // titleCase("WHAT HAPPENS HERE") //should return "What Happens Here"
 // titleCase("") //should return ""
 // titleCase("A") //should return "A"
