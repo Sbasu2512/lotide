b81c18a486b0b27bc0874ad7730c00f911c4b742
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 20:27:30 -0400

    better

--- middle.js
+++ middle.js
@@ -1,27 +1,5 @@
 const eqArrays = require('./eArrays');
-// function eqArrays(a,b) {
-//   if(a.length!=b.length)
-//   return false;
-//  else
-//  {
-//  // comapring each element of array
-//   for(var i=0;i<a.length;i++)
-//   if(a[i]!==b[i])
-//    return false;
-//    return true;
-//  }
-// }
-//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
-//Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison
 const assertArraysEqual = require('./assertArraysEqual');
-
-// function assertArraysEquals (v1,v2){
-//   if(eqArrays(v1,v2)){
-//     return true;
-//   }else{
-//     return false;
-//   }
-// }
 //Implement middle which will take in an array and return the middle-most element(s) of the given array.
 //The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
 const middle = (arr) => {
