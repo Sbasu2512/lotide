e5ec9beb69e6c98bcf622e17bc1d8b4e82e9910a
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 20:47:13 -0400

    join the string to console log the result

--- titleCase.js
+++ titleCase.js
@@ -1,7 +1,8 @@
 //Title case (or Capital Case) is a specific method of capitalizing the characters of every word in a sentence in order to make it usable as a title or headline.
 const titleCase = str => {
   const newStr = str.trim().split(' ');
-  console.log(newStr);
+  //console.log(newStr);
+  return newStr.join('');
 }
 
-titleCase("This is A example");
+console.log(titleCase("This is A example"));
