8e5d98432ccd031e3cc1ef9c0501a114fc688abd
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 21:41:29 -0400

    one more test added

--- test/titleCaseTest.js
+++ test/titleCaseTest.js
@@ -2,14 +2,19 @@ const assert = require('chai').assert;
 const titleCase = require('../titleCase.js');
 
 describe("#titleCase", () => {
+  //console.log(titleCase("THIS is a exAMPle")); should return This Is An Example
   it("should return This Is An Example", () => {
   const input = titleCase("this is an example");
   const output = "This Is An Example";
   assert.equal(input,output);
   });
+  // titleCase("this is an example") //should return "This Is An Example"
+it("should return This Is An Example", () => {
+const input = titleCase("this is an example");
+const output = "This Is An Example";
+assert.equal(input,output);
 });
-//console.log(titleCase("THIS is a exAMPle"));
-// titleCase("this is an example") //should return "This Is An Example"
+});
 // titleCase("test") //should return "Test"
 // titleCase("i r cool") //should return "I R Cool"
 // titleCase("WHAT HAPPENS HERE") //should return "What Happens Here"
