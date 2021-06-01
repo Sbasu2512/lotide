d1c4abbcf0ce751602efd167a530f2a5480d0fd0
Sayantan Basu <sayantanbasu@live.in>
2021-05-31 20:45:48 -0400

    initial foundation being laid

---
+++ titleCase.js
@@ -0,0 +1,7 @@
+//Title case (or Capital Case) is a specific method of capitalizing the characters of every word in a sentence in order to make it usable as a title or headline.
+const titleCase = str => {
+  const newStr = str.trim().split(' ');
+  console.log(newStr);
+}
+
+titleCase("This is A example");
